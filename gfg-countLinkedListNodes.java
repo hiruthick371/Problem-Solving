9f2092e0fee11ff022a7a33e5d4a35676f0d6885
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2024-09-04 23:04:49 +0530

    Add files via upload

---
+++ gfg-countLinkedListNodes.java
@@ -0,0 +1,14 @@
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        // code here
+        
+        int count=0;
+        Node temp=head;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+}
