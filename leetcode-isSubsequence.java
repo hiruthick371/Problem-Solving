bded1fe3413b54ff2c6c2a10d6777f113c2d3c2a
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2024-09-16 22:08:11 +0530

    Add files via upload

---
+++ leetcode-isSubsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int first=0;
+        int second=0;
+        while(first<s.length()&&second<t.length()){
+            if(s.charAt(first)==(t.charAt(second))){
+                first++;
+                second++;
+            }
+            else{
+                second++;
+            }
+        }
+        return first==s.length();
+    }
+}
