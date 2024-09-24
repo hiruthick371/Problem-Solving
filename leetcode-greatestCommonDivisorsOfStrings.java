7380dff36c2744c92b45841bb2ba54b31bf1d0db
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2024-09-24 21:14:07 +0530

    Add files via upload

---
+++ leetcode-greatestCommonDivisorsOfStrings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!((str1+str2).equals(str2+str1))){
+            return "";
+        }
+        int gcd=len(str1.length(),str2.length());
+        return str1.substring(0,gcd);
+    }
+    int len(int a,int b){
+        if(b==0) return a;
+        return len(b,a%b);
+    }
+}
