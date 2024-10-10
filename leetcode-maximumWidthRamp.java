bd74b4aa3f57f328dfd60e4d0bb454c4ea688a59
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2024-10-10 09:08:49 +0530

    Add files via upload

---
+++ leetcode-maximumWidthRamp.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+        int right = 0;
+        int left = 0;
+        int width = 0;
+        while (right < n) {
+            while (left < right && nums[left] > rightMax[right]) {
+                left++;
+            }
+            width = Math.max(right - left, width);
+            right++;
+        }
+        return width;
+    }
+}
