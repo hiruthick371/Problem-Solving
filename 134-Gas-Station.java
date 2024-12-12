f1d4c743760eb9282f0d0dae24e696f9cf184f5b
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2024-12-12 11:58:15 +0530

    [134] [Time Beats: 97.43%] [Memory Beats: 34.74%] - LeetPush

--- 134-Gas-Station.java
+++ 134-Gas-Station.java
@@ -1,7 +1,11 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int costSum = Arrays.stream(cost).sum();
-        int gasSum = Arrays.stream(gas).sum();
+        int costSum = 0;
+        int gasSum = 0;
+        for(int i=0;i<cost.length;++i){
+            costSum+=cost[i];
+            gasSum+=gas[i];
+        }
         if(gasSum<costSum) return -1;
         int diff=0; int start=0;
         for(int i=0;i<cost.length;++i){
