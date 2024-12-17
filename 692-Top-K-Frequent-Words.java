fd20cf4f3ca698dbbd368db51a256d7d7f66c4e6
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2024-12-17 23:17:40 +0530

    [692] [Time Beats: 5.04%] [Memory Beats: 65.05%] - LeetPush

---
+++ 692-Top-K-Frequent-Words.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String,Integer>map=new TreeMap<>();
+        List<String>list = new ArrayList<>();
+        for(String word:words){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+        for(int i=0;i<k;++i){
+            String maxWord =;
+            int times = 0;
+            for(String word:map.keySet()){
+                if(map.get(word)>times){
+                    times = map.get(word);
+                    maxWord = word;
+                }
+            }
+            list.add(maxWord);
+            map.put(maxWord,0);
+        }
+        return list;
+    }
+}
