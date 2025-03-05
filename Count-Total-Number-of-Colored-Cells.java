e49e1613128f8f7c455c03e39cc69a715dea5f80
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2025-03-05 10:39:46 +0530

    [2579] [Time Beats: 5.74%] [Memory Beats: 5.33%] - LeetPush

---
+++ Count-Total-Number-of-Colored-Cells.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long coloredCells(int n) {
+        long[]ans = new long[n];
+        ans[0] = 1;
+        for(int i = 1; i<n; ++i){
+            ans[i] = (ans[i-1] + 4 * i);
+        }
+        return ans[n-1];
+    }
+}
