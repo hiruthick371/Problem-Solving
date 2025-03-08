031766e58862872ac36b468216b3119d65f6d719
Hiruthick B S <142024413+hiruthick371@users.noreply.github.com>
2025-03-08 23:10:34 +0530

    [2379] [Time Beats: 92.10%] [Memory Beats: 86.78%] - LeetPush

--- Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks.java
+++ Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks.java
@@ -1,13 +1,24 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int min = Integer.MAX_VALUE;
-        int blacks = 0;
-        for(int i = 0; i<=blocks.length()-k; ++i) {
-          for(int j = i; j<i+k; ++j) {
-            if(blocks.charAt(j) == 'W') blacks++;
-          }
-          min = Math.min(blacks, min);
-          blacks = 0;
+         int min = Integer.MAX_VALUE;
+         int whites = 0;
+        // for(int i = 0; i<=blocks.length()-k; ++i) {
+        //   for(int j = i; j<i+k; ++j) {
+        //     if(blocks.charAt(j) == 'W') whites++;
+        //   }
+        //   min = Math.min(whites, min);
+        //   whites = 0;
+        // }
+        // return min;
+
+        for(int i = 0; i<k; ++i) {
+            if(blocks.charAt(i) == 'W') whites++;
+        }
+        min = whites;
+        for(int i = k; i<blocks.length(); ++i) {
+            if(blocks.charAt(i-k) == 'W') whites--;
+            if(blocks.charAt(i) == 'W') whites++;
+            min = Math.min(min, whites);
         }
         return min;
     }
